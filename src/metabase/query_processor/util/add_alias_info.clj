ba4049c02df7b0de97576e28abd73a9315ad5b11
ad3lfr9 <112678932+ad3lfr9@users.noreply.github.com>
2023-11-10 21:40:23 +0200

    Update add_alias_info.clj

--- src/metabase/query_processor/util/add_alias_info.clj
+++ src/metabase/query_processor/util/add_alias_info.clj
@@ -352,7 +352,7 @@
   (cond
     join-alias              (prefix-field-alias join-alias (or alias-from-join field-name))
     alias-from-source-query alias-from-source-query
-    :else                   field-name))
+    :else                   (clojure.string/replace field-name #"_id." "")))
 
 (defmulti ^:private clause-alias-info
   {:arglists '([inner-query unique-alias-fn clause])}
